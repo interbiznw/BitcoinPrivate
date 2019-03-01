8086e6fe5e39c7acb0c1f783b51a2a964bd12d0e
J62 <j.owens@interbiznw.com>
2019-02-28 18:14:20 -0800

    put code back in that removed accts but commented out

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -102,6 +102,8 @@ void WalletTxToJSON(const CWalletTx& wtx, UniValue& entry)
 string AccountFromValue(const UniValue& value)
 {
     string strAccount = value.get_str();
+    //if (strAccount != "")	
+    //    throw JSONRPCError(RPC_WALLET_ACCOUNTS_UNSUPPORTED, "Accounts are unsupported");
     return strAccount;
 }
 
